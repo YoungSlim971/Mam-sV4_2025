0450fb620c1942922d0599dddce8d1d2a60d2644
Young Slim <alan.ee971@gmail.com>
2025-07-08 10:03:50 +0200

    Implement client validation in CreerFactureUseCase and add tests

--- Facturation/UseCases/CreerFactureUseCase.swift
+++ Facturation/UseCases/CreerFactureUseCase.swift
@@ -9,7 +9,7 @@ struct CreerFactureUseCase {
     let repository: FactureRepository
 
     func execute(client: ClientModel, tva: Double = 20.0) throws -> FactureModel {
-        // guard client.isValid else { throw FactureError.invalidClient } // TODO: Add validation
+        guard client.isValidModel else { throw FactureError.invalidClient }
         guard tva >= 0 else { throw FactureError.invalidTVA }
         let numero = try repository.genererNumeroFacture(client: client)
         let facture = try repository.createFacture(client: client, numero: numero)
